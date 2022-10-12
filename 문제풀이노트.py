d5faa4c6e7a22fd1509af9500e47f5b221ff55b6
JungJiBum <2377325@naver.com>
2022-10-12 17:50:07 +0900

    upload_programmers

--- 문제풀이노트.py
+++ 문제풀이노트.py
@@ -19,12 +19,29 @@
 
 # print(sol([2100,3200,2100,800],10000))
 
-def solution(arr, k):
-    t = sum(arr,[])
-    t.sort(reverse=True)
-    print(t[k])
+# def solution(arr, k):
+#     t = sum(arr,[])
+#     t.sort(reverse=True)
+#     print(t[k])
     
-    answer = 0
-    return answer
+#     answer = 0
+#     return answer
 
-print(solution([[5,12,4,31],[24,13,11,2],[43,44,19,26],[33,65,20,21]],4))
+# print(solution([[5,12,4,31],[24,13,11,2],[43,44,19,26],[33,65,20,21]],4))
+
+'''에라토스테네스의 체'''
+# n=10
+# a = [False,False] + [True]*(n-1)
+# primes=[]
+
+# for i in range(2,n+1):
+#     print(i)
+#     print(a[i])
+    # if a[i]:
+    #     primes.append(i)
+    #     for j in range(2*i, n+1, i):
+    #         a[j] = False
+# print(primes)
+
+'''배열 유사도'''
+# set().intersection()
