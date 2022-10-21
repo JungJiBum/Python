427eb2d4c2802353dfac118a450fa30406625133
JungJiBum <2377325@naver.com>
2022-10-21 16:18:36 +0900

    upload_udemy_day8

--- Udemy_PythonBootcamp/day8_caesar-cipher_2.py
+++ Udemy_PythonBootcamp/day8_caesar-cipher_2.py
@@ -1,14 +1,9 @@
-from hashlib import new
-from turtle import pos
-
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
             'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
@@ -27,8 +22,6 @@ def decrypt(cipher_text,shift_amount):
     print(f"The decoded text is {plain_text}")
 
 if direction == 'encode':
-    encrypt(plain_text=text, shift_amount=shift)
+    encrypt(text,shift)
 elif direction == 'decode':
-    decrypt(cipher_text=text, shift_amount=shift)
-else:
-    print("Wrong Choice")
+    decrypt(text,shift)
