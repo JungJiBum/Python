7caab352e1f811dd6fbda0335e8cfda3e7a718fe
JungJiBum <2377325@naver.com>
2022-10-26 13:39:58 +0900

    upload_Inflearn_

--- CodeTest_Baekjoon/2562.py
+++
@@ -1,5 +0,0 @@
-ls=[]
-for i in range(1,10):
-    ls.append(int(input()))
-print(max(ls))
-print(ls.index(max(ls))+1)
--- Inflearn_Python_Basic_Part1/4장/ex07.py
+++
@@ -1,9 +0,0 @@
-# 사용자로부터 정수를 입력받아 홀/짝 판별 프로그램 작성
-
-number = int(input("정수를 입력하세요 : "))
-
-if number % 2 == 0 :
-    print("짝수")
-else:
-    print("홀수")
-    
