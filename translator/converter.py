76bfcb3d452c62ea66d4fa63d3e8cd077afdcfda
JungJiBum <2377325@naver.com>
2023-03-31 11:55:25 +0900

    converter_language_upload

--- translator/converter.py
+++ translator/converter.py
@@ -1,25 +1,47 @@
 import tkinter as tk
+from tkinter import ttk
 from deepl import deepl
 
-convert = deepl.DeepLCLI("en", "ko")  # -> 영어를 한국어로
+# convert = deepl.DeepLCLI("en", "ko")  # -> 영어를 한국어로
 
 window = tk.Tk()
 
 window.title("Language Translator")
-window.geometry("900x600+100+100")
+window.geometry("850x650+100+100")
 window.resizable(False, False)
 
 # Label 클래스 생성 및 window 상단에 고정
 word_label = tk.Label(window, text="번역할 내용을 입력해주세요", font=("", 20, "bold"))
 translate_label = tk.Label(window, text="번역된 내용 입니다.", font=("", 20, "bold"))
+word_language = tk.Label(window, text="어떤 언어를 사용 하시나요?")
+translate_language = tk.Label(window, text="어떤 언어로 번역 하시나요?")
 
 # Text클래스 생성
 # -> font=("Times",20,"italic") 폰트속성 설정가능 글꼴,크기,기타속성 공란도 가능
 word_text = tk.Text(master=window, wrap="word", height=15, font=("", 12, ""))
-translate_text = tk.Text(master=window, wrap="word",
-                         height=15, font=("", 12, ""))
+translate_text = tk.Text(master=window, wrap="word",height=15, font=("", 12, ""))
+
+# combobox 생성
+items = ["english","korea","japanese"]
+comboBox = ttk.Combobox(window,width=10,height=10,values=items)
+comboBox1 = ttk.Combobox(window,width=10,height=10,values=items)
+comboBox.set("선택")
+comboBox1.set("선택")
 
 # btn 클릭시 동작 정의
+convert = deepl.DeepLCLI("en", "ko")  # -> 영어를 한국어로
+
+def get_language():
+    if comboBox.get() =="english":
+        if comboBox1.get() == "korea":
+            convert = deepl.DeepLCLI("en","ko")
+        if comboBox1.get() == "japanese":
+            convert = deepl.DeepLCLI("en","ja")
+    if comboBox.get() == "korea":
+        if comboBox1.get() == "english":
+            convert = deepl.DeepLCLI("ko","en")
+        elif comboBox1.get() == "japanese":
+            convert = deepl.DeepLCLI("ko","ja")
 
 
 def click_btn(): word_text.insert(1.0, "안녕하세요\n")
@@ -37,6 +59,7 @@ def click_btn3(): print(word_text.get(1.0, "end"))
 
 
 def click_btn4():
+    get_language()
     text = word_text.get(1.0, "end")  # -> str 타입으로 인식 됨, 줄바꿈은 \n로 인식
     ls = [text]  # -> 문자열을 리스트로 변환함
     translate_text.delete(1.0, "end")
@@ -52,14 +75,29 @@ def click_btn4():
 btn_test = tk.Button(master=window, text="번역하기", command=click_btn4)
 
 # 생성한 클래스 위젯들을 pack() 메서드로 배치
-word_label.pack(side="top")
-btn_test.pack(side="top", anchor="e")
-word_text.pack(side="top", anchor="center")
-translate_label.pack(side="bottom", anchor="n")
-translate_text.pack(side="bottom", anchor="s")
+# word_label.pack(side="top")
+# btn_test.pack(side="top", anchor="e")
+# word_text.pack(side="top", anchor="center")
+# translate_label.pack(side="bottom", anchor="n")
+# translate_text.pack(side="bottom", anchor="s")
+word_label.grid(row =0,column=0,columnspan=5)
+word_language.grid(row=0,column=5)
+word_text.grid(row=1,column=0)
+comboBox.grid(row=1, column=5)
+
+translate_label.grid(row=8,column=0,columnspan=5)
+translate_language.grid(row=8,column=5)
+comboBox1.grid(row=9, column=5)
+translate_text.grid(row=9,column=0)
+btn_test.grid(row=10, column=5)
+
 # btn_insert.pack()
 # btn_delete.pack()
 # btn_get.pack()
 
 # 창 유지하기 위한 코드
 window.mainloop()
+'''
+https://edukoi.tistory.com/108
+
+'''
